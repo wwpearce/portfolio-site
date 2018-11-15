bf810692cb0bf6596293a0b38c7ead75d12848e3
Bill Pearce <bill@Bills-MBP.home>
2018-11-15 13:58:19 -0500

    Have watch and serve working. Starting out on an upload task.

--- gulp_tasks/_webpack.js
+++
@@ -1,8 +0,0 @@
-let gulp = require("gulp");
-let webpack = require('webpack-stream');
-
-gulp.task('webpack', () => {
-  return gulp.src('build/')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('build/'));
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,46 @@
 const gulp = require("gulp");
 const webpack = require('webpack-stream');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const shell = require('gulp-shell');
 
 const OUT_DIR = './build';
+const bucket = 's3://billwpearce.com/';
 
 sass.compiler = require('node-sass');
 
 gulp.task('copy:html', () =>
   gulp.src('./app/index.html')
-    .pipe(gulp.dest(OUT_DIR)));
+  .pipe(gulp.dest(OUT_DIR)));
 
 gulp.task('scss', () =>
   gulp.src('./app/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css')));
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./app/css')));
 
 gulp.task('copy:css', ['scss'], () =>
   gulp.src('./app/css/style.css')
-    .pipe(gulp.dest(OUT_DIR)));
+  .pipe(gulp.dest(OUT_DIR)));
 
 gulp.task('copy:assets', () =>
   gulp.src('./app/assets/*')
-    .pipe(gulp.dest(OUT_DIR + '/assets/')));
+  .pipe(gulp.dest(OUT_DIR + '/assets/')));
 
 gulp.task('webpack', () =>
   gulp.src('build/')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('build/')));
+  .pipe(webpack(require('./webpack.config.js')))
+  .pipe(gulp.dest('build/')));
 
-gulp.task('default', ['copy:html', 'copy:css', 'copy:assets', 'webpack']);
+gulp.task('serve', () =>
+  browserSync.init({
+    server: OUT_DIR
+  }));
 
-gulp.task('watch', function () {
-  gulp.watch('./app/scss/**/*.scss', ['default']);
+gulp.task('watch', ['serve'], () => {
+  gulp.watch('./app/scss/**/*.scss', ['copy:css']).on('change', browserSync.reload);
+  gulp.watch('./app/assets/*', ['copy:assets']).on('change', browserSync.reload);
+  gulp.watch('./app/index.html', ['copy:html']).on('change', browserSync.reload);
+  gulp.watch('./app/js/**/*.js', ['webpack']).on('change', browserSync.reload);
 });
+
+gulp.task('default', ['watch']);
