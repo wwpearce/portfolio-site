aae367b6004066c15c275bf720bbc4bbac302613
wwpearce <wwpearce@gmail.com>
2019-05-27 09:29:55 -0700

    Updated copy. Updated grid hover opacity.

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -85,19 +85,25 @@ export default class About extends Component {
         <div class="inner_content-wrapper">
             <div class="inner_content-wrapper_left">
               <p>
-                &#8230; and I'm assuming you're a hiring manager, or a creative director, or my parents, or somebody who needs a website; you know, that type of thing. You're probably looking for a designer or a developer or better yet you just read an article on Creative Technologists and Google brought you here. Which brings us to the crux of the issue.
+                &#8230;and I'm assuming you're a technical director, or a creative director, or somebody who needs a website; you know, that type of thing. You're probably looking for a designer or a developer or both or better yet you just read an article on Creative Technologists and Google brought you here. Which brings us what may be your first question&#8230;
               </p>
-              <h2>WTF is a creative technologist</h2>
-              <p>It's me! I'm a Creative Technologist! I am equally adept at the creative and production phases of a project. I can dream up a product or campaign, slap together a prototype, run a UX study, iterate on designs, and write production quality webcode all by my lonesome. I'm comfortable in all roles along the production timeline, from UX to Art Direction to Front-End Dev. Most of my experience has been in advertising, but I've spent some time at tech companies too. Here’s a link to my <a href="assets/docs/resume_bill-pearce.pdf" target="_blank">resume</a>.</p>
+              <h2>WTF is a creative technologist???</h2>
+              <p>It's me! I'm a Creative Technologist! I am a creative thinker from a technical POV. Equally adept at the creative and production phases of a project, I can dream up a product or campaign, slap together a prototype, run a UX study, iterate on designs, and write production quality webcode all by my lonesome. I'm comfortable in all roles along the production timeline, from UXer to Art Director to Front-End Developer. Most of my experience has been in advertising, but I've spent some time at a certain big tech company too. Here’s a link to my <a href="assets/docs/resume_bill-pearce.pdf" target="_blank">resume</a>.</p>
             </div>
           <div class="inner_content-wrapper_right">
             <p>
-              I'm client friendly and I've spoken at a few conferences on topics ranging from <a href="#">how to run a guerrilla UX study</a> to <a href="#">automation tools for Alexa skill development</a>.<br /><br />
-              I also have a side-hustle designing and illustrating logos for small businesses.
-              I also have a side-side hustle teaching private and group Muay Thai classes.
+              I'm client friendly and I've spoken at a few conferences. Here's a video of my two of my talks:<a href="#">Guerilla-Style User Research</a> to <a href="#">Thou Shalt Not Skill Without a Build Process</a>.
+              <br /><br />
+              I also have a few little side-business in which I design and illustrate logos (there's a few in the work section) and teach <a href="https://en.wikipedia.org/wiki/Muay_Thai">Muay Thai</a>. I am usually pretty good about not mixing those two up so don't feel like I'm gonna head kick you if you ask me for a logo (unless you step out on the bill).
+              <br /><br />
+              Thanks for stopping by. Don't be shy if you wanna reach out!
             </p>
-            <h2>TL ; DR – I'm a designer / developer</h2>
-            <h2 class="selectable-text">billwpearce at gmail dot com</h2>
+            <h4 class="selectable-text">billwpearce at gmail dot com</h4>
+            <div class="socialLinkWrapper">
+              <a href="https://www.linkedin.com/in/billwpearce/" target="_blank">linkedin</a> | <a href="https://github.com/wwpearce" target="_blank">github</a>
+            </div>
+            <h4>TL ; DR – I'm a designer / developer</h4>
+
           </div>
         </div>
       </Plx>
